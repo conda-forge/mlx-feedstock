9d2b26d57b4e05bb373571262f3b3dec4d0ac074
ngam <67342040+ngam@users.noreply.github.com>
2023-12-23 23:57:11 -0500

    enable cross-compilation via archflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ export PYPI_RELEASE=1
 export CMAKE_GENERATOR=Ninja
 export DEVELOPER_DIR=/Applications/Xcode_15.1.app/Contents/Developer
 export CMAKE_BUILD_PARALLEL_LEVEL=""
+export ARCHFLAGS="-arch x86_64 -arch arm64"
 if [[ "${target_platform}" != "osx-arm64" ]]; then
   export BLAS_HOME=$PREFIX
   export CMAKE_ARGS="${CMAKE_ARGS} -DMLX_BUILD_METAL=OFF"
