832f903484bda25fafa58383e84033dbe002d6bb
ngam <67342040+ngam@users.noreply.github.com>
2023-12-24 00:14:27 -0500

    revert forbidden edits

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,12 @@ set -exuo pipefail
 
 export PYPI_RELEASE=1
 export CMAKE_GENERATOR=Ninja
-export DEVELOPER_DIR=/Applications/Xcode_15.1.app/Contents/Developer
+export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 export CMAKE_BUILD_PARALLEL_LEVEL=""
-export ARCHFLAGS="-arch x86_64 -arch arm64"
 if [[ "${target_platform}" != "osx-arm64" ]]; then
   export BLAS_HOME=$PREFIX
   export CMAKE_ARGS="${CMAKE_ARGS} -DMLX_BUILD_METAL=OFF"
+else
+  export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" "
 fi
 $PYTHON -m pip install . -vv
